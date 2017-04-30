dde5e147a42c50357d09a3f9da0e18b4cc61561d
Kevin Seminara <kevinseminara@gmail.com>
2017-04-30 14:29:20 -0700

    add 34

--- ex33.py
+++ ex33.py
@@ -22,7 +22,7 @@ for i in range(0, 6):
     print "At the top i is %d" % i
     numbers.append(i)
 
-    i += 1
+
     print "Numbers now: ", numbers
     print "At the bottom i is %d" % i
 
---
+++ ex34.py
@@ -0,0 +1,7 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+animals.append('zebra')
+i = 0
+while i < len(animals):
+    print animals[i]
+    i += 1
