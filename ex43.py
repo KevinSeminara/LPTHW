bd78cd3986398b8a8da0224f01f704b38c37be5e
kevinseminara <kevinseminara@gmail.com>
2017-05-05 14:09:07 -0700

    add engine death and central corridor to 43

--- ex43.py
+++ ex43.py
@@ -1,25 +1,58 @@
+from sys import exit
+from random import randint
+
 class Scene(object):
 
     def enter(self):
-        pass
+        print "This scene is not yet configured. Subclass it and implement enter()."
+        exit(1)
 
 class Engine(object):
 
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
 
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        # be sure to print out the last scene
+        current_scene.enter()
+
 
 def Death(Scene):
 
+    quips = [
+        "You died. Sorry!",
+        "Too bad that you are dead.",
+        "Good bye cruel world"
+    ]
+
     def enter(self):
-        pass
+        print Death.quips[randint(0, len(self.quips)-1)]
+        exit(1)
 
 class CentralCorridor(Scene):
 
     def enter(self):
-        pass
+        print "You are in the Central Corridor"
+        print "An enemy jumps out in front of you!"
+        action = raw_input("shoot or run?> ")
+
+        if action == "shoot":
+            print "You miss because you're a bad shot"
+            return 'death'
+        elif action == "run":
+            print "He lunges at you but misses, you leave quickly"
+            return 'laser_weapon_armory'
+        else:
+            print "Does not compute!"
+            return 'central_corridor'
+
 
 class LaserWeaponArmory(Scene):
 
