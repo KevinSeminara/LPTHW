698f91c360f7fc067175e5d3363f33a8a114831a
kevinseminara <kevinseminara@gmail.com>
2017-05-12 14:03:08 -0700

    added and started 45

---
+++ ex45.py
@@ -0,0 +1,21 @@
+import random
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+inputword = raw_input('Enter word> ')
+#inputguess = raw_input('Enter guess> ')
+totalcounter = 0
+rightcounter = 0
+fullmatch = False
+
+inputlist = list(inputword)
+guesslist = []
+
+while fullmatch == False:
+    letterguess = ''
+
+
+
+
+    totalcounter += 1
+
