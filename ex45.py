5a4e57f081fbf5929a5f9a6fa5f781e5e0168864
kevinseminara <kevinseminara@gmail.com>
2017-05-16 14:50:45 -0700

    Completed 45

--- ex45.py
+++ ex45.py
@@ -1,21 +1,35 @@
 import random
+import time
 
-letters = 'abcdefghijklmnopqrstuvwxyz'
+letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 inputword = raw_input('Enter word> ')
 #inputguess = raw_input('Enter guess> ')
 totalcounter = 0
 rightcounter = 0
 fullmatch = False
+lettermatch = False
 
 inputlist = list(inputword)
 guesslist = []
-
-while fullmatch == False:
-    letterguess = ''
+printlist = []
 
 
+while rightcounter < len(inputword):
+    guesslist = []
+    for each in inputword:
+        guesslist.append(random.choice(letters))
+
+    if guesslist[rightcounter] == inputword[rightcounter]:
+        printlist.append(guesslist[rightcounter])
+        rightcounter += 1
+        print ''.join(printlist) + ''.join(guesslist[rightcounter:])
+    elif len(printlist) == 0:
+        print ''.join(guesslist)
+    else:
+        print ''.join(printlist) + ''.join(guesslist[rightcounter:])
+    time.sleep(.030)
 
 
-    totalcounter += 1
+
 
